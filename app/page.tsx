c5862c7e1b57e88f2608f95c85156b3124c86b5c
Tanishq Solanki <officialxinov@gmail.com>
2024-01-01 00:11:34 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,97 @@
-import Image from 'next/image'
+'use client';
+
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+
+import Snowfall from 'react-snowfall';
+import ReactPlayer from 'react-player';
+import Image from "next/image";
 
 export default function Home() {
+  const [topVal, setTopVal] = useState('top-32');
+  const [bottomVal, setBottomVal] = useState('top-0');
+
+  const [isNewYear, setNewYear] = useState(false);
+
+  const snowflake1 = document.createElement('img')
+  snowflake1.src = 'sflake.png';
+  const snowflake2 = document.createElement('img')
+  snowflake2.src = 'sflake.png';
+
+  const images = [snowflake1, snowflake2]
+
+  const newYear = () => {
+    setTopVal('top-0');
+    setBottomVal('-top-64');
+  }
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+
+  useEffect(() => {
+    setTimeout(() => {
+      newYear();
+
+      setNewYear(true);
+    }, 3000);
+  }, [isNewYear])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Snowfall
+        images={images}
+        radius={[16, 28]}
+        snowflakeCount={50}
+      />
+      <Snowfall />
+
+      <main className="flex w-full min-h-screen flex-col items-center justify-between p-20">
+        <div className="flex justify-center items-center w-full h-full">
+          <div className="flex justify-between items-center w-[22rem] h-32 overflow-y-hidden text-[10rem] font-black text-yellow-200  text">
+            <div className="flex justify-start items-center w-full">
+              20
+            </div>
+
+            <div className="relative flex justify-end items-center w-full">
+              <div className={`absolute z-[1] ${bottomVal} bottom-0 right-0 flex justify-end items-center w-full duration-500`}>
+                23
+              </div>
+
+              <div className={`absolute z-[2] ${topVal} bottom-0 right-0 flex justify-end items-center w-full duration-500`}>
+                24
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="flex justify-center items-center w-full mt-4 text-center text-white font-bold text-xl">
+          Tanxin & Nixnat wishes you a <br /> HAPPY NEW YEAR
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="flex justify-center items-center w-full mt-4 text-center text-white font-bold text-xl">
+          featuring 9th A & 3TAV
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex justify-center items-center w-full mt-4 text-center text-white font-bold text-xl">
+          <Image className="flex justify-center items-center w-80 h-80"
+            width={100}
+            height={100}
+            src={'https://pusheen.com/wp-content/uploads/2022/12/Happy_New_Year_2023.gif'}
+            alt=""
+          />
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <ReactPlayer url={'s1.mp3'} playing={true} />
+        <ReactPlayer url={'s2.mp3'} playing={isNewYear} />
+        <ReactPlayer url={'s3.mp3'} playing={true} />
+        <ReactPlayer url={'s4.mp3'} playing={true} />
+        <ReactPlayer url={'s5.mp3'} playing={true} />
+        <ReactPlayer url={'s6 .mp3'} playing={true} />
+      </main>
+    </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pusheen.com",
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig
